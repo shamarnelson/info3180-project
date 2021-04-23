81d22f94d2724e78a0d365cb5cd86a9504866650
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2021-04-23 13:53:43 -0500

    updated files

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from app import db
 from werkzeug.security import generate_password_hash
+from flask_login._compat import unicode
 
 
 
 class Cars(db.Model):
-
+    __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255))
     make = db.Column(db.String(80))
@@ -17,13 +18,44 @@ class Cars(db.Model):
     photo = db.Column(db.String(80))
     user_id = db.Column(db.Integer)
 
+    def __init__(self, description, make, model, colour, year, transmission, car_type, price, photo, user_id ):
+        self.description = description
+        self.make = make
+        self.model = model
+        self.colour = colour
+        self.year= year
+        self.transmission=transmission
+        self.car_type= car_type
+        self.price= price
+        self.photo= photo
+        self.user_id= user_id
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
 class Favourites(db.Model):
+    __tablename__ = 'favourites'
 
     id = db.Column(db.Integer, primary_key=True)
     car_id = db.Column(db.Integer)
     user_id = db.Column(db.Integer)
 
+    def __init__(self, car_id, user_id ):
+        self.car_id= car_id
+        self.user_id= user_id
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
 class Users(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(255))
@@ -33,6 +65,36 @@ class Users(db.Model):
     biography = db.Column(db.String(80))
     photo = db.Column(db.String(80))
     date_joined = db.Column(db.DateTime)
+
+    def __init__(self, username, password, name, email, location, biography, photo,date_joined):
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+        self.name = name
+        self.email = email
+        self.location= location
+        self.biography=biography
+        self.photo= photo
+        self.date_joined= date_joined
+
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,58 @@
 from werkzeug.utils import secure_filename
-import os
-from app import app,db
-from flask import render_template,request,redirect,url_for,flash,jsonify
-from app.forms import registerform,loginform
-import datetime
+import os,jwt, datetime
+from app import app,db, login_manager
+from flask import render_template,request,redirect,url_for,flash,jsonify,send_from_directory, session, abort, g, make_response
+from app.forms import registerform,loginform, CarForm, SearchForm
 from werkzeug.security import check_password_hash
+from functools import wraps
+from app.config import *
+from app.models import Users,Cars,Favourites
+
+def requires_auth(f):
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.headers.get('Authorization', None) # or request.cookies.get('token', None)
+
+        if not auth:
+            return jsonify({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'}), 401
+
+        parts = auth.split()
+
+        if parts[0].lower() != 'bearer':
+
+            return jsonify({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}), 401
+        elif len(parts) == 1:
+            return jsonify({'code': 'invalid_header', 'description': 'Token not found'}), 401
+        elif len(parts) > 2:
+            return jsonify({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}), 401
+
+        token = parts[1]
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+
+        except jwt.ExpiredSignatureError:
+            return jsonify({'code': 'token_expired', 'description': 'token is expired'}), 401
+        except jwt.DecodeError:
+            return jsonify({'code': 'token_invalid_signature', 'description': 'Token signature is invalid'}), 401
+
+        g.current_user = user = payload
+        return f(*args, **kwargs)
+
+    return decorated
+
+###
+# Routing for your application.
+###
+@app.route('/api/secure', methods=['GET'])
+@requires_auth
+def api_secure():
+    # This data was retrieved from the payload of the JSON Web Token
+    # take a look at the requires_auth decorator code to see how we decoded
+    # the information from the JWT.
+    user = g.current_user
+    return jsonify(data={"user": user}, message="Success")
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -95,4 +143,127 @@ def login():
 def logout():
     logout_user()
     return jsonify({'message':"you are now logged out"}) 
+
+@app.route('/api/cars', methods=['POST','GET'])
+def car():
+    form=CarForm()
+    
+    if request.method == 'POST' and form.validate_on_submit():
+            make=request.form['make']
+            model=request.form['model']
+            colour=request.form['colour']
+            year=request.form['year']
+            price=request.form['price']
+            cartype=request.form['cartype']
+            trans=request.form['transmission']
+            desc=request.form['description']
+            pic=request.files['pic'] 
+            filename=secure_filename(pic.filename)
+            car=Cars(make=make,model=model,colour=colour, year=year, transmission=trans,car_type=cartype,price=price,description=desc,photo=filename,user_id=g.current_user["userid"])
+            if car is not None:
+                db.session.add(car)
+                db.session.commit()
+                pic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                jsonmsg=jsonify(message="Successful")         
+                return jsonmsg
+            else:
+                err=form_errors(form)
+                jsonErr=jsonify(errors=err)
+                return jsonErr
+            
+    if request.method == 'GET':
+        allc=[]
+        cars=Cars.query.order_by(Cars.id).all()
+        for c in cars:
+            car={}
+            car['id']=c.id
+            car["user_id"]=c.user_id
+            car["year"]=c.year        
+            car["price"]=c.price
+            car["photo"]=c.photo
+            car["make"]=c.make
+            car["model"]=c.model
+            allc.append(car)
+            return jsonify(allcars=allc,test=g.current_user["userid"])
+        
+@app.route('/api/cars/<car_id>', methods=['GET'])
+def car_details(car_id):       
+    if request.method == 'GET':
+        c=Cars.query.filter_by(id=car_id).first()
+        return jsonify(id=c.id,model=c.model,make=c.make,user_id=c.user_id,car_type=c.car_type,
+            description=c.description,price=c.price,photo=c.photo,
+            transmission=c.transmission,colour=c.colour,year=c.year)
+
+@app.route('/api/cars/<car_id>/favourite', methods=['POST'])
+def favourite_car(car_id):       
+    if request.method == 'POST':
+        userid=g.current_user['userid']
+        fav=Favourites(car_id,userid)
+        db.session.add(fav)
+        db.session.commit()
+        return jsonify(message="Car added as Favourite!")
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def user_details(user_id):       
+    if request.method == 'GET':
+        u=Users.query.filter_by(id=user_id).first()
+        user={}
+        user['id']=u.id
+        user['username']=u.username
+        user['name']=u.name
+        user['email']=u.email
+        user["location"]=u.location
+        user["biography"]=u.biography        
+        user["photo"]=u.photo
+        user["date_joined"]=u.date_joined
+        return jsonify(user=user)
+
+@app.route('/api/users/<user_id>/favourites', methods=['GET'])
+def user_favourites(user_id):       
+    if request.method == 'GET':
+        favcars=[]
+        favouritecar=Favourites.query.filter_by(user_id=user_id).all()
+        for x in favouritecar:
+            c=Cars.query.filter_by(id=x.car_id).first()
+            car={}
+            car['id']=c.id
+            car["user_id"]=c.user_id
+            car["year"]=c.year        
+            car["price"]=c.price
+            car["photo"]=c.photo
+            car["make"]=c.make
+            car["model"]=c.model
+            favcars.append(car)
+        return jsonify(favcars=favcars)
+
+@app.route('/api/search',methods=["GET"])
+def car_search():
+    search=[]
+    if request.method=="GET":
+        make=request.args.get('searchbymake')
+        model=request.args.get('searchbymodel')
+        search_cars= Cars.query.filter((Cars.make.like(make)|Cars.model.like(model)))
+        for i in search_cars:
+            car={}
+            car['id']=i.id
+            car["user_id"]=i.user_id
+            car["year"]=i.year            
+            car["price"]=i.price
+            car["photo"]=i.photo
+            car["make"]=i.make
+            car["model"]=i.model
+            search.append(car)
+        return jsonify(searchedcars=search)
+  
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="8080")
     
